3ec20f3c05c8f2b589a7cd6269a2b4f32a4bc287
Jarkko Tulensalo <jarkko.tulensalo@hotmail.com>
2019-11-12 20:29:54 +0200

    basic Graph class structure

--- test.py
+++ test.py
@@ -3,6 +3,13 @@ from graph import Graph
 
 graph = Graph(fname="ca-GrQc.txt",
               fpath="",
-              algorithm="")
+              algorithm="",
+              k=2)
 
 graph.draw_map()
+
+graph.partition_graph()
+
+graph.draw_partition_map()
+
+graph.write_output()
